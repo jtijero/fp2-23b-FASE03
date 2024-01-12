6d46e9256eae2a8bc91b2c533d715ee7a4be3fb1
aquispearr <aquispearr@unsa.edu.pe>
2024-01-11 23:20:37 -0500

    Modificamos la clase Book agregando el atributo para el numero de ejemplares y el metodo para obtenerlos por sucursal

--- fp2-23b FASE03/Book.java
+++ fp2-23b FASE03/Book.java
@@ -1,23 +1,30 @@
 public class Book extends Article {
 	String ISBN;
-	
+	int copiesAvailable;
 	
 	public Book(String title) {
 		super(title);
 		super.setType((byte) 1);
 	}
 	
-	public Book(String code, String title, int pages) {
-		super(code,title,pages);
-		super.setType((byte) 1);
-	}
-	
-	public String getISBN() {
-		return ISBN;
-	}
+	public Book(String code, String title, int pages, int copiesAvailable) {
+        	super(code, title, pages);
+        	this.copiesAvailable = copiesAvailable;
+        	super.setType((byte) 1);
+    	}
+
+
 	public void setISBN(String iSBN) {
 		ISBN = iSBN;
 	}
+	
+	public int getCopiesAvailable() {
+        	return copiesAvailable;
+        }
+
+        public void setCopiesAvailable(int copiesAvailable) {
+        	this.copiesAvailable = copiesAvailable;
+    	}
 	
 	
 }
